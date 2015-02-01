bffbc3d49090fa099c955fb188c56d18131cf23c
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-02-01 07:23:50 +0000

    [2/2] QS: Screenshot Tile
    Change-Id: Ieb3eddd45be2a8c6c4bf19b5931c40998794324d
    
    Conflicts:
    	res/values/strings.xml
    	src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
    
    Conflicts:
    	res/values/slimmod_strings.xml
    	src/com/android/settings/slim/qs/QSTileHolder.java

--- src/com/android/settings/slim/qs/QSTileHolder.java
+++ src/com/android/settings/slim/qs/QSTileHolder.java
@@ -144,6 +144,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_usb_tether_on";
                 stringId = R.string.qs_tile_usb_tether;
                 break;
+            case QSConstants.TILE_SCREENSHOT:
+                resourceName = "ic_qs_screenshot";
+                stringId = R.string.qs_screenshot_tile;
+                break;
             case QSConstants.TILE_SLIMACTION:
                 resourceName = "ic_qs_reboot_recovery";
                 stringId = R.string.qs_tile_slimaction;
